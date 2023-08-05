fc98d2927348f22e3b35f3cc9cb7f708ee0ada22
Dom <dtaplin21@aol.com>
2023-08-05 11:39:33 -0700

    seray

--- public/index.js
+++ public/index.js
@@ -1,1 +1,7 @@
-// Your code here
+window.onload = (event) => {
+
+    const div = document.createElement("div");
+    const h1 = document.createElement("h1");
+    h1.innerText = "Kitten pic"
+    div.appendChild("h1")
+}
